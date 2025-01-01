e45fdf25699d0aa87c7c9391c1c5e592c9c1136c
MohanManirul <fiforeg@gmail.com>
2025-01-01 20:48:06 +0600

    my changes

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 
-echo "hello";
+echo "hello my changes";
 
 ?>
 
 
 
 
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/MohanManirul/myProject.git
-git push -u origin main
+
+
